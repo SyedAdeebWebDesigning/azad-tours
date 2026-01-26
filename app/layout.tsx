33743992ae423747010ea85f921251ebeb00f790
Syed Adeeb <abidi9897401278@gmail.com>
2026-01-27 01:00:42 +0530

    Under Development: Added Basic Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,36 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Azad Tours and Travels",
-  description: "Explore the world with Azad Tours and Travels. Your adventure starts here.",
+	title: "Azad Tours and Travels",
+	description:
+		"Explore the world with Azad Tours and Travels. Your adventure starts here.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+				{children}
+				<Analytics />
+			</body>
+		</html>
+	);
 }
